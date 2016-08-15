29b990c28f48a224bd1417e93dc64664732c36ab
Boris Mann <boris@bmannconsulting.com>
2016-08-14 21:54:54 -0700

    Switching to single user mode -- just showing bmann posts

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -12,7 +12,7 @@ $app['theme'] = 'foundation6';
 // what type of posts should we load?
 $app['steem'] = [
   // an array of users allowed to appear
-  'accounts' => ['jesta', 'dantheman', 'ned', 'bmann'],
+  'accounts' => ['bmann'],
   // an array of tags to ensure exist
   'tags' => [
     'steemstats',
