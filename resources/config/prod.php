8fe0d936efd95e92863b85d4fa3a481fdb195231
Boris Mann <boris@bmannconsulting.com>
2016-08-15 23:11:47 -0700

    Copied over _config file into twig/steempress format

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -7,7 +7,7 @@ $app['locale'] = 'en';
 $app['steemd'] = 'https://steem.steemstats.com';
 
 // theme configuration
-$app['theme'] = 'mediator';
+$app['theme'] = 'sosimple';
 
 // what type of posts should we load?
 $app['steem'] = [
@@ -20,7 +20,39 @@ $app['steem'] = [
 ];
 
 $app['site'] = [
-  'title' => 'Boris Mann on Steem',
-  'author' => 'Boris Mann',
-  'url' => 'http://bmann-steempress.herokuapp.com'
+
+# Site wide configuration
+
+'title' => 'Site Title',
+# locale: en_US
+'description' => 'Describe your website here.',
+'logo' => 'site-logo.png',
+# 'search' => true,
+# 'url' => 'localhost:3000',
+'url' => 'http://bmann-steempress.herokuapp.com',
+
+# Site owner
+# owner:
+  'name' => 'Boris Mann',
+  'avatar' => 'bio-photo.jpg',
+  'email' => 'boris@bmannconsulting.com',
+  'disqus-shortname' => '',
+  'steemit' => 'bmann', #username
+  'twitter' => 'bmann', #username
+  'facebook' => 'bmann', #username
+  'github' => 'bmann', #username
+  # stackexchange: #http://stackoverflow.com/users/123456/username
+  # linkedin: #username
+  # instagram: #username
+  # flickr: #username
+  # tumblr: #username
+  # pinterest: #username
+  # weibo: #userid
+  # google:
+  #  plus: #username
+  #  analytics:
+  #  verify:
+  #  ad-client:
+  #  ad-slot:
+  # bing-verify:
 ];
